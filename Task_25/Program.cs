5d4f34e8fa3734edcbf30d238e1461fb643bb2e0
yana sarvarova <serg.yana@mail.ru>
2022-07-05 22:55:18 +0300

    Задача 25

---
+++ Task_25/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите цикл, который принимает на вход 2 натуральных числа (А и В) 
+и возводит число А в степень В */
+
+Zadacha30();
+
+void Zadacha30()
+{
+    Console.WriteLine("Введите число А");
+    int numberA = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число B больше нуля");
+    int numberB = Convert.ToInt32(Console.ReadLine());
+    if (numberB >0) DegreeNumber(numberA, numberB);
+    else Console.WriteLine("Число В введено неверно");
+}
+
+void DegreeNumber (int numA, int numB)
+{
+    int result = 1;
+    for (int i = 0; i < numB; i++ )
+    {
+        result = result * numA;
+    }
+    Console.WriteLine($"Число {numA} в степени {numB} равно {result}");
+}
